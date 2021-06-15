e3101ac07e23dabccde59beaaa3fb003c462060b
Jonathan Ng <jonathanng@Jonathans-MacBook-Pro.local>
2021-06-15 15:36:48 -0700

    Added Jonathan's Seaside Bakery guide

---
+++ jonathan/script.js
@@ -0,0 +1,32 @@
+// var toggle = false;
+
+
+// function onClick() {
+
+//     if (!toggle) {
+//         document.getElementById("greeting").innerHTML = "hi bud, today is " + Date();
+//         toggle = true;
+//     } else {
+//         document.getElementById("greeting").innerHTML = "";
+//         toggle = false;
+//     }
+    
+//     // alert("Hello")
+// }
+
+// function generateWord() {
+//     var welcome = document.createElement('h1');
+//     welcome.innerHTML = "W E L C O M E";
+//     welcome.id = "welcome";
+//     welcome.style.position = 'absolute';
+//     var SOME_RANDOM_NUMER = Math.floor(Math.random()*100)+1;
+//     var SOME_OTHER_RANDOM_NUMBER = Math.floor(Math.random()*100)+1;
+
+//     welcome.style.left = SOME_RANDOM_NUMER + '%';
+//     welcome.style.top = SOME_OTHER_RANDOM_NUMBER + '%';
+//     document.getElementById('soup').appendChild(welcome);
+// }
+
+// window.onload = () => {
+//     generateWord();
+// }
