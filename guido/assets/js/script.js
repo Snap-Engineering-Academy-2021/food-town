3841e8138c94c5ee22e6ccf2bab34b6d951e5bbf
Guido Asbun <guidoasbun@Guidos-MBP.localdomain>
2021-06-16 11:27:27 -0700

    updated ingredient objects

--- guido/assets/js/script.js
+++ guido/assets/js/script.js
@@ -1,31 +1,46 @@
 'use strict'
 
-const displayIngredients = document.querySelector('.displayIngredients');
+const displayVegies = document.querySelector('.displayVegies');
+const displayLandMeats = document.querySelector('.displayLandMeats')
 const displayInstructions = document.querySelector('.displayInstructions');
 
-const ingredients = [
+const ingredientsVegies = [
     { type: 'vegies', amountS: 1, amountM: 2, amountL: 3, name: "Red Pepper"},
     { type: 'vegies', amountS: 1, amountM: 2, amountL: 3, name: 'Green Pepper'},
     { type: 'vegies', amountS: 1, amountM: 2, amountL: 3, name: "Medium White Onion"},
     { type: 'vegies', amountS: 1, amountM: 2, amountL: 3, name: "Pinch Parsley"},
+]
+const ingredientsMeatsLand = [
     { type: 'meats', amountS: 1, amountM: 2, amountL: 3, name: "lbs Chicken"},
     { type: 'meats', amountS: 1, amountM: 2, amountL: 3, name: "Spanish Chorizo"},
+]
+const ingredientsMeatsSea = [
     { type: 'meats-sea', amountS: 6, amountM: 10, amountL: 14, name: "Muscles"},
     { type: 'meats-sea', amountS: 6, amountM: 10, amountL: 14, name: "Prawns"},
     { type: 'meats-sea', amountS: 6, amountM: 10, amountL: 14, name: "clams"},
+]
+const ingredientsSeasoning = [
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "Pinch Salt"},
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "Pinch Pepper"},
     { type: 'seasoning', amountS: 1, amountM: 2, amountL: 3, name: "Pinch Saphron"},
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "tbs Olive Oil"},
+]
+const ingredientsOther = [
     { type: 'other', amountS: 2, amountM: 4, amountL: 6, name: "lbs Bomba Rice"},
     { type: 'other', amountS: 2, amountM: 4, amountL: 6, name: "oz Chicken Stock"},
-
 ]
 
-for (let i = 0; i < ingredients.length; ++i) {
-    displayIngredients.innerHTML += `<div class="ingredient-display">${ingredients[i].amountS} -- ${ingredients[i].name}</div>`;
+const displayIngrdients = function (type) {
+    for (let i = 0; i < `ingredients${type}`.length; ++i) {
+        `display${type}`.innerHTML += `<div class="ingredient-display">${`ingredients${type}[i]`.amountS} -- ${ingredientsVegies[i].name}</div>`
+    }
+    
 }
 
+for (let i = 0; i < ingredientsVegies.length; ++i) {
+    displayVegies.innerHTML += `<div class="ingredient-display">${ingredientsVegies[i].amountS} -- ${ingredientsVegies[i].name}</div>`;
+}
 
+displayIngrdients('LandMeats')
 
 
