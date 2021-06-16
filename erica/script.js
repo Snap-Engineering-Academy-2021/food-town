2a2149298ff750a241bdf562f68cb0d9feeb4023
Erica Carballo <erica@Ericas-MacBook-Pro.local>
2021-06-15 23:26:01 -0700

    Added pizza and pasta buttons and allowed text to show up when the buttons are pressed. Buttons are not aligned perfectly yet

---
+++ erica/script.js
@@ -0,0 +1,38 @@
+var toggle = false;
+
+function onClickPizza()
+{
+    if (!toggle) 
+    {
+        document.getElementById("pizza").innerHTML = "This pizza is topped with BBQ sauce," +
+        " smoked Gouda, red onions and fresh cilantro. You also have the option to top it with" +
+        " bacon and pineapple. I highly suggest the pineapple. Once you are finished, for dessert," +
+        " you can have the Butter Cake, which goes very well with this pizza.";
+        toggle = true;
+    } else 
+    {
+        document.getElementById("pizza").innerHTML = "";
+
+        toggle = false;
+    }
+
+}
+
+
+function onClickPasta()
+{
+    if (!toggle) 
+    {
+        document.getElementById("pasta").innerHTML = "If you are not completely " +
+        "crazy about the pizza, this is the perfect dish for you. This dish is " +
+        "a pasta that has fresh Italian parsley, and an option to top it with " +
+        "chicken and/or shrimp, and Sauteed Cremini Mushrooms. This dish also " +
+        "goes very well with the Butter Cake."
+        toggle = true;
+    } else 
+    {
+        document.getElementById("pasta").innerHTML = "";
+        toggle = false;
+    }
+
+}
