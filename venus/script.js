bd5dcfa3094ea2dd728eb801e654fba1d0377291
Venus Nguyen <venusnguyen@Venuss-MacBook-Pro.local>
2021-06-16 15:38:12 -0700

    Fulfill the recipe

--- venus/script.js
+++ venus/script.js
@@ -1,3 +1,11 @@
-function onClick() {
-    document.getElementById("greeting").innerHTML = "Hey there!!!";
-}
+var toggle = true;
+
+function openNutritionFact() {
+    if (toggle) {
+        document.getElementById("nutrition-facts").innerHTML = "Per Serving: 345 calories; protein 4.9g; carbohydrates 29.4g; fat 23.5g; cholesterol 86.4mg; sodium 51.5mg.";
+        toggle = false;
+    } else {
+        document.getElementById("nutrition-facts").innerHTML = "";
+        toggle = true;
+    }
+};
