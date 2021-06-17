8debed1f6630cbc6774118d41533077e2648287b
guidoasbun <guido@asbun.io>
2021-06-16 22:05:00 -0700

    finished ingredients section

--- guido/assets/js/script.js
+++ guido/assets/js/script.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const inputButtons = document.querySelectorAll('button');
-const displayVegies = document.querySelector('.displayVegies');
-const displayLandMeats = document.querySelector('.displayLandMeats');
-const displaySeaMeats = document.querySelector('.displaySeaMeats');
-const displaySeasonings = document.querySelector('.displaySeasoning')
+const displayVegies = document.getElementById('displayVegies');
+const displayLandMeats = document.getElementById('displayLandMeats');
+const displaySeaMeats = document.getElementById('displaySeaMeats');
+const displaySeasonings = document.getElementById('displaySeasoning')
 const displayInstructions = document.querySelector('.displayInstructions');
 
 const ingredientsVegies = [
@@ -26,7 +26,7 @@ const ingredientsSeaMeats = [
 const ingredientsSeasonings = [
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "Pinch Salt"},
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "Pinch Pepper"},
-    { type: 'seasoning', amountS: 1, amountM: 2, amountL: 3, name: "Pinch Saphron"},
+    { type: 'seasoning', amountS: 1, amountM: 2, amountL: 3, name: "Pinch Saffron"},
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "tbs Olive Oil"},
     { type: 'seasoning', amountS: 2, amountM: 4, amountL: 6, name: "oz Chicken Stock"},
 ]
@@ -43,41 +43,41 @@ inputButtons.forEach((inputButton) => {
 const displayIngredients = function () {
     for (let i = 0; i < ingredientsVegies.length; ++i) {
         if(size === 'L') {
-            displayVegies.innerHTML += `<div class="ingredient-display">${ingredientsVegies[i].amountL} -- ${ingredientsVegies[i].name}</div>`;
+            displayVegies.innerHTML += `<div>${ingredientsVegies[i].amountL} -- ${ingredientsVegies[i].name}</div>`;
         }else if (size === 'M') {
-            displayVegies.innerHTML += `<div class="ingredient-display">${ingredientsVegies[i].amountM} -- ${ingredientsVegies[i].name}</div>`;
+            displayVegies.innerHTML += `<div>${ingredientsVegies[i].amountM} -- ${ingredientsVegies[i].name}</div>`;
         }else {
-            displayVegies.innerHTML += `<div class="ingredient-display">${ingredientsVegies[i].amountS} -- ${ingredientsVegies[i].name}</div>`;
+            displayVegies.innerHTML += `<div>${ingredientsVegies[i].amountS} -- ${ingredientsVegies[i].name}</div>`;
         }
     }
     
     for (let i = 0; i < ingredientsLandMeats.length; ++i) {
         if(size === 'L') {
-            displayLandMeats.innerHTML += `<div class="ingredient-display">${ingredientsLandMeats[i].amountL} -- ${ingredientsLandMeats[i].name}</div>`;
+            displayLandMeats.innerHTML += `<div>${ingredientsLandMeats[i].amountL} -- ${ingredientsLandMeats[i].name}</div>`;
         }else if (size === 'M') {
-            displayLandMeats.innerHTML += `<div class="ingredient-display">${ingredientsLandMeats[i].amountM} -- ${ingredientsLandMeats[i].name}</div>`;
+            displayLandMeats.innerHTML += `<div>${ingredientsLandMeats[i].amountM} -- ${ingredientsLandMeats[i].name}</div>`;
         }else {
-            displayLandMeats.innerHTML += `<div class="ingredient-display">${ingredientsLandMeats[i].amountS} -- ${ingredientsLandMeats[i].name}</div>`;
+            displayLandMeats.innerHTML += `<div>${ingredientsLandMeats[i].amountS} -- ${ingredientsLandMeats[i].name}</div>`;
         }
     }
     
     for (let i = 0; i < ingredientsSeaMeats.length; ++i) {
         if(size === 'L') {
-            displaySeaMeats.innerHTML += `<div class="ingredient-display">${ingredientsSeaMeats[i].amountL} -- ${ingredientsSeaMeats[i].name}</div>`;
+            displaySeaMeats.innerHTML += `<div>${ingredientsSeaMeats[i].amountL} -- ${ingredientsSeaMeats[i].name}</div>`;
         }else if (size === 'M') {
-            displaySeaMeats.innerHTML += `<div class="ingredient-display">${ingredientsSeaMeats[i].amountM} -- ${ingredientsSeaMeats[i].name}</div>`;
+            displaySeaMeats.innerHTML += `<div>${ingredientsSeaMeats[i].amountM} -- ${ingredientsSeaMeats[i].name}</div>`;
         }else {
-            displaySeaMeats.innerHTML += `<div class="ingredient-display">${ingredientsSeaMeats[i].amountS} -- ${ingredientsSeaMeats[i].name}</div>`;
+            displaySeaMeats.innerHTML += `<div>${ingredientsSeaMeats[i].amountS} -- ${ingredientsSeaMeats[i].name}</div>`;
         }
     }
     
     for (let i = 0; i < ingredientsSeasonings.length; ++i) {
         if(size === 'L') {
-            displaySeasonings.innerHTML += `<div class="ingredient-display">${ingredientsSeasonings[i].amountL} -- ${ingredientsSeasonings[i].name}</div>`;
+            displaySeasonings.innerHTML += `<div>${ingredientsSeasonings[i].amountL} -- ${ingredientsSeasonings[i].name}</div>`;
         }else if (size === 'M') {
-            displaySeasonings.innerHTML += `<div class="ingredient-display">${ingredientsSeasonings[i].amountM} -- ${ingredientsSeasonings[i].name}</div>`;
+            displaySeasonings.innerHTML += `<div>${ingredientsSeasonings[i].amountM} -- ${ingredientsSeasonings[i].name}</div>`;
         }else {
-            displaySeasonings.innerHTML += `<div class="ingredient-display">${ingredientsSeasonings[i].amountS} -- ${ingredientsSeasonings[i].name}</div>`;
+            displaySeasonings.innerHTML += `<div>${ingredientsSeasonings[i].amountS} -- ${ingredientsSeasonings[i].name}</div>`;
         }
     }
 }
