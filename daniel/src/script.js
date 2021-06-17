e7fefe986de4510e309ad985441ede9c1ef3ddf4
Daniel Addison <Daniel.addison1@marist.edu>
2021-06-17 17:43:15 -0400

    Banish all the .Ds_Store

--- daniel/src/script.js
+++ daniel/src/script.js
@@ -1,3 +1,24 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+// document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
 
-console.log
+// console.log
+
+// Recipe calculator with jquery
+var computeServing = function(serving) {
+    $('.js-recipeIngredient').each(function(index, item) {
+      $(item).children('span').html($(item)[0].dataset.basevalue * serving)
+    })
+  }
+  $('#servingInput').on('change', function() {
+    computeServing($(this).val())
+  })
+  $('.js-decreaseService').on('click', function() {
+    var currentServing = $('#servingInput').val()
+    $('#servingInput').val(currentServing - 1)
+    computeServing(currentServing - 1)
+  })
+  $('.js-increaseService').on('click', function() {
+    var currentServing = $('#servingInput').val()
+    $('#servingInput').val(parseInt(currentServing) + 1)
+    computeServing(parseInt(currentServing) + 1)
+  })
+  computeServing(1)
