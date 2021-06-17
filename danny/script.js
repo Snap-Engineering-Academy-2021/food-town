f0e7c8e2c4d96292a5465c934b2d8306448a8f2d
Daniel Addison <Daniel.addison1@marist.edu>
2021-06-17 18:43:08 -0400

    last commit

---
+++ danny/script.js
@@ -0,0 +1,36 @@
+// document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+// console.log
+
+// Recipe calculator with jquery
+var computeServing = function(serving) {
+    $('.js-recipeIngredient').each(function(index, item) {
+      $(item).children('span').html($(item)[0].dataset.basevalue * serving)
+    })
+  }
+  $('#servingInput').on('change', function() {
+    computeServing($(this).val())
+  })
+  $('.js-decreaseService').on('click', function() {
+    var currentServing = $('#servingInput').val()
+    $('#servingInput').val(currentServing - 1)
+    computeServing(currentServing - 1)
+  })
+  $('.js-increaseService').on('click', function() {
+    var currentServing = $('#servingInput').val()
+    $('#servingInput').val(parseInt(currentServing) + 1)
+    computeServing(parseInt(currentServing) + 1)
+  })
+  computeServing(1)
+
+  //collapsible
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
