48d0286d0da3eda5aa6bd4add5852da60a4a7588
Phoenix Ngan <phoenix@Phoenixs-MacBook-Pro.local>
2021-06-17 00:26:26 -0700

    Finshed CSS and Added Button with Javascript Functions

---
+++ phoenix/javascript.js
@@ -0,0 +1,11 @@
+function myFunction(){
+  window.open("https://snap-engineering-academy-2021.github.io/food-town/");
+}
+
+function onHover(){
+  document.getElementById("button").style.backgroundColor = "hotpink";
+}
+
+function offHover(){
+  document.getElementById("button").style.backgroundColor = "green";
+}
